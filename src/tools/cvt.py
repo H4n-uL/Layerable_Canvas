87a7de0a20f4505b2fb9a9475d0e7b43cbb5eebf
h4n-ul <jun061119@gmail.com>
2024-04-05 17:44:01 +0900

    f

--- src/tools/cvt.py
+++ src/tools/cvt.py
@@ -4,6 +4,11 @@ import os
 
 sRGB_Profile = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sRGB.icc')
 
+D50TOD65 = np.array([
+[ 0.9554734527042182,  -0.023098536874261423, 0.0632593086610217],
+[-0.028369706963208136, 1.0099954580058652,   0.021041398966943008],
+[0.012314001688319899, -0.020507696433477912, 1.3303659366080753]])
+
 def RGBAtoLACA(data: np.ndarray, alpha: np.ndarray, profile):
     if profile is None: profile = open(sRGB_Profile, 'rb').read()
     rXYZ = build_matrix.get_cXYZ(profile, b'r')
@@ -21,10 +26,13 @@ def RGBAtoLACA(data: np.ndarray, alpha: np.ndarray, profile):
 
     retdata = []
     while data.size > 0:
-        temp = np.transpose(data[0], (1, 0))[::-1]
-        temp[0] = temp[0]**rTRC[0]
-        temp[1] = temp[1]**gTRC[0]
-        temp[2] = temp[2]**bTRC[0]
-        retdata.append(np.vstack((RGB2XYZ.dot(temp), alpha[0])))
+        # BGR to RGB
+        row = np.transpose(data[0], (1, 0))[::-1]
+        row[0] = row[0]**rTRC[0]
+        row[1] = row[1]**gTRC[0]
+        row[2] = row[2]**bTRC[0]
+        row = RGB2XYZ.dot(row)
+        row = D50TOD65.dot(row)
+        retdata.append(np.vstack((row, alpha[0])))
         data = data[1:]; alpha = alpha[1:]
     return np.array(retdata)
