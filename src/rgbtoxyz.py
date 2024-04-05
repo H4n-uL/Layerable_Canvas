3acbeccacc6a6088aabe679253a77edcc45775a6
h4n-ul <jun061119@gmail.com>
2024-04-05 17:26:29 +0900

    ㅁ

--- src/rgbtoxyz.py
+++ src/rgbtoxyz.py
@@ -20,7 +20,6 @@ open('image.laca', 'wb').write(b'')
 for img, index in zip(images, range(len(images))):
     image = Image.open(img).convert('RGBA')
     profile = image.info.get('icc_profile')
-    # open('profile.icc', 'wb').write(image.info.get('icc_profile'))
     alpha = np.array(image)[..., 3]
     roff = dt.get(str(alpha.dtype), 1)
     alpha = alpha.astype(np.float32) / roff
@@ -31,17 +30,7 @@ for img, index in zip(images, range(len(images))):
     resolution = data.shape[:2]
 
     xyza = cvt.RGBAtoLACA(data[..., :3], alpha, profile)
-    print(data)
-    print(xyza)
-    trnsp = np.transpose(xyza, (0, 2, 1))[..., :3].astype(np.float32)
-    print(trnsp[787][429])
-    # print(cv2.cvtColor(trnsp, cv2.COLOR_RGB2XYZ))
-    # plt.imshow(cv2.cvtColor(trnsp, cv2.COLOR_RGB2XYZ))
-    # plt.show()
 
-    # xyz = cv2.cvtColor(data[..., :3], cv2.COLOR_XYZ2RGB)
-
-    # xyza = np.dstack((xyz, alpha))
     open('image.laca', 'ab').\
         write(build.layer('image', {}, 1.0, index, (resolution[0], resolution[1]), (1, 1), ('0', '0'), '0', False, False))
     for rno in range(len(xyza)):
